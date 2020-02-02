78bb2801a4d5e6a4cbdf77b82514eef10118d19b
Christina Li <christinali@ChristinasMBP3.fritz.box>
2020-02-02 14:45:17 +1300

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-tutorial-1010"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://1zn04jkyog.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_3nyX1nGWh",
+        APP_CLIENT_ID: "61vsb0hjgf74nggjod1vbcvlou",
+        IDENTITY_POOL_ID: "us-east-2:d139c00e-95f3-475f-9d6d-6cd50e1c579a"
+    }
+};
